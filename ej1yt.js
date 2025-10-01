acef1b4796a2bbefb1095e1a32f99258d368d8e6
karel59117 <karelmartinezq@hotmail.com>
2025-10-01 17:07:00 -0600

    framework ej2

--- ej1yt.js
+++ ej1yt.js
@@ -3,9 +3,9 @@ Feature('tutorial');
 Scenario('buscar',  ({ I }) => {
     I.amOnPage("/");
     I.fillField('//*[@id="center"]/yt-searchbox/div[1]/form/input' , "Tutorial playwright");
-    I.wait(5);
+    I.wait(3);
     I.pressKey("Enter");
-    I.wait(5);
+    I.wait(3);
 });
 
 
@@ -13,8 +13,8 @@ Feature('reproducir video codeceptJS');
 
 Scenario('reproducir',  ({ I }) => {
     I.amOnPage("https://www.youtube.com/results?search_query=Tutorial+de+CodeceptJS");
-    I.wait(5);
+    I.wait(3);
     I.click('//*[@id="contents"]/yt-lockup-view-model[1]/div/a/yt-collection-thumbnail-view-model/yt-collections-stack/div/yt-thumbnail-view-model/div/img');
-    I.wait(7);
+    I.wait(5);
 });
 
